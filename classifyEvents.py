c74afec0aeb4e13e2aeddebc88ddb194ef2671f5
Juanda Tan <Juanda@Juandas-MacBook-Pro.local>
2017-04-12 13:04:44 -0400

    classified fb events and cleaned rent/income data

---
+++ classifyEvents.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import sys
+import csv
+import argparse
+from collections import defaultdict
+
+import util
+
+import numpy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from tokenizer import Tokenizer
+
+def load_training_file(file_path):
+	art_ = []
+	texts = []
+	with open(file_path, 'r', encoding='latin1') as file_reader:
+		reader = csv.reader(file_reader, delimiter=',', quotechar='"')
+		next(reader, None)
+		for row in reader:
+			sentiment = int(row[2])
+			text = row[1]
+			art_.append(sentiment)
+			texts.append(text)
+	return (art_, texts)
+
+def load_input_file(file_path):
+	features = []
+	locations = []
+	with open(file_path, 'r', encoding='latin1') as file_reader:		
+		reader = csv.reader(file_reader, delimiter=',', quotechar='"')
+		next(reader, None)
+		for row in reader:
+			locations.append(row[0])
+			features.append(row[6])
+	return (locations, features)
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-training', required=True, help='Path to training data')
+	parser.add_argument('-input', required=True, help='Input file with features to classify')
+	opts = parser.parse_args()
+
+	tokenizer = Tokenizer()
+	vectorizer = CountVectorizer(binary=True, lowercase=True, decode_error='replace', tokenizer=tokenizer)
+
+	# Load training text and training labels
+	(training_labels, training_texts) = load_training_file(opts.training)
+
+	# Get training features using vectorizer
+	training_features = vectorizer.fit_transform(training_texts)
+
+	# Transform training labels to numpy array (numpy.array)
+	training_labels = numpy.array(training_labels)
+	############################################################
+
+	##### TRAIN THE MODEL ######################################
+	# Initialize the type of the classifier
+	classifier = BernoulliNB(binarize=None)
+
+	# Train classifier using 'fit'
+	classifier.fit(training_features, training_labels)
+
+	(input_locations, input_text) = load_input_file(opts.input)
+	input_features = vectorizer.transform(input_text)
+	predicted_labels = classifier.predict(input_features)
+
+	with open("ClassifiedFacebookEvents.csv", "w") as classified:
+		writer = csv.writer(classified)
+		writer.writerow(["neighborhood", "description", "art_event_indicator"])
+		for i in range(0, len(predicted_labels)):
+			writer.writerow([input_locations[i], input_text[i], predicted_labels[i]])
+
+if __name__ == '__main__':
+	main()
---
+++ cleanHousingData.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import csv
+import argparse
+import re
+
+def main():
+	zip_to_subborough = {}
+	with open("data/districtLocationInfo2.csv", "r") as district_info:
+		reader = csv.reader(district_info)
+		next(reader, None)
+		for row in reader:
+			zipcodes = row[2].split(", ")
+			for zipcode in zipcodes:
+				zip_to_subborough[zipcode] = row[0]
+	getMedianRentChange(zip_to_subborough)
+	getAverageIncomeChange(zip_to_subborough)
+	
+def getMedianRentChange(zip_to_subborough):	
+	change_by_zip = {}
+	with open("data/median rent/median_rent_2015.csv", "r") as median_15:
+		median_2015 = csv.reader(median_15)
+		next(median_2015, None)
+		next(median_2015, None)
+		for row in median_2015:
+			if row[3] != "-":
+				median_rent_temp = row[3].replace("+", "")
+				median_rent = median_rent_temp.replace("+", "")
+				change_by_zip[row[1]] = int(median_rent)
+
+	with open("data/median rent/median_rent_2011.csv", "r") as median_11:
+		median_2011 = csv.reader(median_11)
+		next(median_2011, None)
+		next(median_2011, None)
+		for row in median_2011:
+			if row[3] != "-":
+				median_rent_temp = row[3].replace("+", "")
+				median_rent = median_rent_temp.replace(",", "")
+				if row[1] in change_by_zip:
+					change_by_zip[row[1]] -= int(median_rent)
+
+	median_rent_by_subborough = {}
+	for zipcode in zip_to_subborough:
+ 		median_rent_by_subborough[zip_to_subborough[zipcode]] = [0, 0]
+
+	for zipcode in change_by_zip:
+ 		if zipcode in zip_to_subborough:
+	 		subborough = zip_to_subborough[zipcode]
+	 		median_rent_by_subborough[subborough][0] += change_by_zip[zipcode]
+	 		median_rent_by_subborough[subborough][1] += 1
+
+	with open("averageMedianRentChangeBySubborough.csv", "w") as output:
+		writer = csv.writer(output)
+		writer.writerow(["subborough", "2011-2015 average change in median rent price"])
+		for subborough in median_rent_by_subborough:
+			writer.writerow([subborough, str(median_rent_by_subborough[subborough][0] / median_rent_by_subborough[subborough][1])])
+
+
+def getAverageIncomeChange(zip_to_subborough):
+	avg_income_by_subborough_2015 = {}
+	for zipcode in zip_to_subborough:
+ 		avg_income_by_subborough_2015[zip_to_subborough[zipcode]] = [0, 0]
+
+	to_exclude = []	
+	with open("data/average income/income_2015.csv", "r") as units_2015:
+		with open("data/average income/aggregate_income_2015.csv", "r") as avg_2015:
+			housing_units_2015 = csv.reader(units_2015)
+			avg_income_2015 = csv.reader(avg_2015)
+			next(housing_units_2015, None)
+			next(housing_units_2015, None)
+			next(avg_income_2015, None)
+			next(avg_income_2015, None)
+
+			for row in avg_income_2015:
+				if row[3] == "-":
+					to_exclude.append(row[1])
+			for row in housing_units_2015:
+				if row[3] == "-":
+					to_exclude.append(row[1])
+
+	with open("data/average income/income_2015.csv", "r") as units_2015:
+		with open("data/average income/aggregate_income_2015.csv", "r") as avg_2015:
+			housing_units_2015 = csv.reader(units_2015)
+			avg_income_2015 = csv.reader(avg_2015)
+			next(housing_units_2015, None)
+			next(housing_units_2015, None)
+			next(avg_income_2015, None)
+			next(avg_income_2015, None)
+
+			for row in avg_income_2015:
+				if row[1] in zip_to_subborough and row[1] not in to_exclude:
+					avg_income_by_subborough_2015[zip_to_subborough[row[1]]][0] += int(row[3])
+						
+			for row in housing_units_2015:
+				if row[1] in zip_to_subborough and row[1] not in to_exclude:
+					avg_income_by_subborough_2015[zip_to_subborough[row[1]]][1] += int(row[3])
+			
+
+	for subborough in avg_income_by_subborough_2015:
+		avg_income_by_subborough_2015[subborough] = avg_income_by_subborough_2015[subborough][0] / avg_income_by_subborough_2015[subborough][1]
+
+	avg_income_by_subborough_2011 = {}
+	for zipcode in zip_to_subborough:
+ 		avg_income_by_subborough_2011[zip_to_subborough[zipcode]] = [0, 0]
+
+	to_exclude = []	
+	with open("data/average income/income_2011.csv", "r") as units_2011:
+		with open("data/average income/aggregate_income_2011.csv", "r") as avg_2011:
+			housing_units_2011 = csv.reader(units_2011)
+			avg_income_2011 = csv.reader(avg_2011)
+			next(housing_units_2011, None)
+			next(housing_units_2011, None)
+			next(avg_income_2011, None)
+			next(avg_income_2011, None)
+
+			for row in avg_income_2011:
+				if row[3] == "-":
+					to_exclude.append(row[1])
+			for row in housing_units_2011:
+				if row[3] == "-":
+					to_exclude.append(row[1])
+
+	with open("data/average income/income_2011.csv", "r") as units_2011:
+		with open("data/average income/aggregate_income_2011.csv", "r") as avg_2011:
+			housing_units_2011 = csv.reader(units_2011)
+			avg_income_2011 = csv.reader(avg_2011)
+			next(housing_units_2011, None)
+			next(housing_units_2011, None)
+			next(avg_income_2011, None)
+			next(avg_income_2011, None)
+
+			for row in avg_income_2011:
+				if row[1] in zip_to_subborough and row[1] not in to_exclude:
+					avg_income_by_subborough_2011[zip_to_subborough[row[1]]][0] += int(row[3])
+						
+			for row in housing_units_2011:
+				if row[1] in zip_to_subborough and row[1] not in to_exclude:
+					avg_income_by_subborough_2011[zip_to_subborough[row[1]]][1] += int(row[3])
+			
+
+	for subborough in avg_income_by_subborough_2011:
+		avg_income_by_subborough_2011[subborough] = avg_income_by_subborough_2011[subborough][0] / avg_income_by_subborough_2011[subborough][1]
+
+	avg_income_change_by_subborough = {}
+	for subborough in avg_income_by_subborough_2015:
+		if subborough in avg_income_by_subborough_2011:
+			avg_income_change_by_subborough[subborough] = avg_income_by_subborough_2015[subborough] - avg_income_by_subborough_2011[subborough]
+
+	with open("averageIncomeChangeBySubborough.csv", "w") as avg_change:
+		average_change = csv.writer(avg_change)
+		average_change.writerow(["subborough", "2011-2015 change in average income"])
+		for subborough in avg_income_change_by_subborough:
+			average_change.writerow([subborough, avg_income_change_by_subborough[subborough]])
+
+if __name__ == '__main__':
+	main()
